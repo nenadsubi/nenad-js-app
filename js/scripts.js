b854c89aee09d14a37609ccf16d275b8a7fc1e58
nenadsubi <sube_dube@yahoo.com>
2025-04-02 00:17:05 -0400

    add
    create pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-alert('Hello world');
+let pokemonList = [
+  {name: 'Bulbasaur', Height: 7, type: ['Grass', 'Monster']},
+  {name: 'Pikachu', Height: 4, type: 'Electric'},
+  {name: 'Raticate', Height: 7, type: 'Normal'}
+];

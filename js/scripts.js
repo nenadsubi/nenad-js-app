2b60e285213421861b8dc5a6d3add3a1a0315245
nenadsubi <sube_dube@yahoo.com>
2025-04-30 14:31:06 -0400

    update
    Conditional height pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
+// Declaration of pokemon array with each object
 let pokemonList = [
-  {name: 'Bulbasaur', Height: 7, type: ['Grass', 'Monster']},
-  {name: 'Pikachu', Height: 4, type: 'Electric'},
-  {name: 'Raticate', Height: 7, type: 'Normal'}
+  {name: 'Bulbasaur',
+     Height: 7, 
+     type: ['Grass', 'Monster']},
+  {name: 'Pikachu',
+     Height: 4, 
+     type: ['Electric']},
+  {name: 'Raticate',
+     Height: 7,
+      type: ['Normal']}
 ];
+
+// For loop to print out the details of each pokemon 
+ for (  let i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + $`{pokemonList [i].Height}` + "</p")
+ }
+ // For loop to print out which pokemon is the biggest
+ for (  let i = 0; i < pokemonList.length;  i++) {
+   if (pokemonList[i].Height >7) {
+      document.write("<p>"  +  `${pokemonList[i].name} Wow, that's big` + "</p")
+   }
+ }
+ 
